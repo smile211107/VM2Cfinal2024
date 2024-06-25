c3c5ea791446b29590dbf680e993ae914a343df5
BlackJackMC <duytu85343@gmail.com>
2024-06-25 15:26:27 +0700

    Update input.py: now can read general file(maybe)

--- input.py
+++ input.py
@@ -1,20 +1,29 @@
 import sys
 
-sys.stdin = open("./data/2.3/Facility.txt", "r")
-sys.stdout = open("./data/2.3/Facility.csv", "w")
-print("id, x, y, capacity, cost")
-for i in range(10):
-    print(input().replace("\t", ", "))
 
+def txt_to_csv(filenameWithoutExtension, header):
+    inp = sys.stdin
+    out = sys.stdout
+    header_len = 0
 
-sys.stdin = open("./data/2.3/DemandAdd.txt", "r")
-sys.stdout = open("./data/2.3/DemandAdd.csv", "w")
-print("id, x, y, require")
-for i in range(200):
-    print(input().replace("\t", ", "))
+    sys.stdin = open(f"{filenameWithoutExtension}.txt", 'r')
+    sys.stdout = open(f"{filenameWithoutExtension}.csv", 'w')
 
+    if header:
+        header_len = len(header)
+        print(", ".join(header))
+    while True:
+        try:
+            temp = input()
+            if temp.count("\t") < header_len - 1:
+                raise RuntimeError("Number of header is not enough")
+            print(temp.replace("\t", ","))
+        except EOFError:
+            break
 
-sys.stdin = open("./data/2.3/TransCost.txt", "r")
-sys.stdout = open("./data/2.3/TransCost.csv", "w")
-for i in range(10):
-    print(input().replace("\t", ", "))
+    sys.stdin = inp
+    sys.stdout = out
+
+txt_to_csv("./data/2.5/Facility", ["id", "x", "y", "capacity", "cost"])
+txt_to_csv("./data/2.5/DemandRand", ["id", "x", "y", "avg", "std"])
+txt_to_csv("./data/2.5/TransCost", None)
